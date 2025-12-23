e887a2aa63ca27eafd79516b9f109f0977ba7217
SriCharan28 <117657170+SriCharan28@users.noreply.github.com>
2025-12-23 15:34:47 +0530

    Clean up counter_trans.sv by removing comments
    Removed commented out UVM macros and adjusted formatting.

--- UVM/write_agent/counter_trans.sv
+++ UVM/write_agent/counter_trans.sv
@@ -9,17 +9,7 @@ rand bit [3:0] data_in;
 bit [3:0] data_out;
 
 static int trn_id;
-
-/*
-`uvm_object_utils_begin(counter_trans)
-	`uvm_field_int(rst,UVM_ALL_ON)
-	`uvm_field_int(mode,UVM_ALL_ON)
-	`uvm_field_int(load,UVM_ALL_ON)
-	`uvm_field_int(data_in,UVM_ALL_ON)
-	`uvm_field_int(data_out,UVM_ALL_ON)
-`uvm_object_utils_end
-*/
-
+	
 constraint data_range {data_in inside {[0:11]};}
 constraint reset_freq {rst dist {0:=4,1:=1};}
 constraint mode_freq {mode dist {0:=2,1:=2};}
@@ -29,7 +19,6 @@ function new(string name = "counter_trans");
 	super.new(name);
 endfunction
 
-///*
 function void do_copy(uvm_object rhs);
 	counter_trans th;
 	if(!$cast(th,rhs))
@@ -62,9 +51,9 @@ function void do_print(uvm_printer printer);
 	printer.print_field("RESET",rst,1,UVM_UNSIGNED);
 	printer.print_field("MODE",mode,1,UVM_UNSIGNED);	
 	printer.print_field("LOAD",load,1,UVM_UNSIGNED);
-	printer.print_field("DATA_IN",data_in,4,UVM_UNSIGNED);	printer.print_field("DATA_OUT",data_out,4,UVM_UNSIGNED);
+	printer.print_field("DATA_IN",data_in,4,UVM_UNSIGNED);	
+	printer.print_field("DATA_OUT",data_out,4,UVM_UNSIGNED);
 endfunction
-//*/
 
 function void post_randomize();
 	trn_id++;
