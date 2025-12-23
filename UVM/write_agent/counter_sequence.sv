87f84b43f0044371eb063dd1c83971a9c5c2da23
SriCharan28 <117657170+SriCharan28@users.noreply.github.com>
2025-12-23 15:34:14 +0530

    Refactor counter_sequence class definition

--- UVM/write_agent/counter_sequence.sv
+++ UVM/write_agent/counter_sequence.sv
@@ -18,22 +18,4 @@ class counter_sequence extends uvm_sequence#(counter_trans);
 
 endclass
 
-class extended_counter_sequence extends counter_sequence;
 
-	`uvm_object_utils(extended_counter_sequence);
-
-	function new(string name = "extended_counter_sequence");
-		super.new(name);
-	endfunction
-	
-	task body();
-		repeat(no_of_trans)
-		begin
-			req=counter_trans::type_id::create("req");
-			start_item(req);
-			assert(req.randomize() with {load==1; data_in=={2,8,9};});
-			finish_item(req);
-		end
-	endtask
-
-endclass
